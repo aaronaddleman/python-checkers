458e1c0774496ad805871db99500fb38e8153d5a
Aaron Addleman <aaronaddleman@gmail.com>
2022-02-21 10:35:50 -0800

    Add tests for color of two squares

--- tests/test_items.py
+++ tests/test_items.py
@@ -16,4 +16,13 @@ def gameboard():
 
 def test_gameboard(gameboard):
     assert isinstance(gameboard, GameBoard)
+    gameboard.build_board()
+    # get the color of a squre
+    white_square = gameboard.grid[0][0].get('color')
+    # get the color of a squre
+    black_square = gameboard.grid[3][0].get('color')
+    # test the color value of the squares of
+    # what they should be based on the size of the board
+    assert white_square == "white"
+    assert black_square == "black"
 
