6f1d7b4dc696ca07cc139d8dcd042900c80be7fe
Aaron Addleman <aaronaddleman@gmail.com>
2022-02-21 10:12:51 -0800

    Fix gameboard testing
    Cleaned up testing of test_items.py to verify the board object

--- tests/test_items.py
+++ tests/test_items.py
@@ -2,25 +2,18 @@ import os
 import sys
 from unittest import TestCase
 
+import pytest
 from checkers.items import GameBoard, Player
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 
-# @pytest.fixture
-# def two_player_game():
-#     """Test a two player game"""
-#     return Game()
+@pytest.fixture
+def gameboard():
+    """Test a two player game"""
+    game = GameBoard(total_rows=8, total_cols=8)
+    return game
 
-
-class TryTesting(TestCase):
-    def test_always_passes(self):
-        self.assertTrue(True)
-
-    def test_two_player_game(self):
-        game = GameBoard(total_rows=8, total_cols=8)
-        assert isinstance(game, GameBoard)
-
-        player_rafael = Player(name="Rafael")
-        assert isinstance(player_rafael, Player)
+def test_gameboard(gameboard):
+    assert isinstance(gameboard, GameBoard)
 
