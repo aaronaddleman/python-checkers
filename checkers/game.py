9594337a5737b095b96966fae1ee843a9460ca39
Aaron Addleman <aaronaddleman@gmail.com>
2022-02-21 13:27:34 -0800

    Remove unused code
    This code is not needded at this time. Was trying to go to far in the the future
    and decided to rely on testing before going this fast.

--- checkers/game.py
+++ checkers/game.py
@@ -7,18 +7,5 @@ class Game:
 
     def __init__(self, total_players=2, board_size=8):
         """Set a game up with players and board_size."""
-        self.total_players = total_players
-        self.board_size = GameBoard(x=board_size, y=board_size)
-        self.players = []
-        self.turn_history = []
-
-    def draw_board(self):
-        print("Drawing the board")
-
-    def player_turn(self):
-        return {"player_name": "harry",
-                "translation": {"src": {"row": 2, "col": 1},
-                                "dst": {"row": 3, "col": 2}}}
-
-    def player_takes_turn(self):
-        self.turn_history.append(self.player_turn())
+        self.gameboard = GameBoard()
+        self.gameboard.build_board()
