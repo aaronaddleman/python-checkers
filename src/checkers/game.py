1553e1810c65ffd104a05e3a5e3213803d6550a8
Aaron Addleman <aaronaddleman@gmail.com>
2022-05-17 21:25:45 -0700

    adding start_game and print_board with numbers and possible_moves
    this change adds 3 things to the program:
    
    1. to start the game really fast with predefined players
    2. print the board with coordinate
    3. list possible moves with selected row and column

--- src/checkers/game.py
+++ src/checkers/game.py
@@ -10,3 +10,12 @@ class Game:
         self.gameboard = GameBoard()
         self.gameboard.build_board()
 
+    def start_game(self):
+        """Start the game and see who wins"""
+        # add players
+        player1black = Player(color="black")
+        player2white = Player(color="white")
+        self.gameboard.add_player_pieces(player_black=player1black, player_white=player2white)
+        # print the board
+        self.gameboard.print_board()
+
